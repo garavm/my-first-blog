7ae14fa98d7c729fefa61b5ecd44e16ae5a9b6cf
garavm <mohapatragaurav9@gmail.com>
2017-12-05 14:51:50 +0530

    added host

--- myfirstdpro/settings.py
+++ myfirstdpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'smg&f1tpcscw#oj)lzd8fgkc4*7d_+h)&!040%w&g#kg+pjx1j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8081', 'epsumlabs.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', "epsumlabs.pythonanywhere.com"];
 
 
 # Application definition
